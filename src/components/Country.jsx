f93b434039e9a325f999cd5fcff89eb346f2d0bb
EM-Hash <emeade3@my.wctc.edu>
2022-08-19 10:27:32 -0500

    - Let user add or remove medals

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,33 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMedal } from '@fortawesome/free-solid-svg-icons';
+import { faMedal, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Button, Typography, Grid, Box } from '@mui/material';
 import React, { Component } from 'react';
-import { Container } from '@mui/system';
 
 class Country extends Component {
 
-    state = {
-        name: this.props.name,
-        medals: this.props.medals,
-    }
-
     render() { 
-        let medals = this.state.medals.map((medal) => {
-            return (
-                <Container key={medal.name + "Medal"}>
-                    <FontAwesomeIcon icon={faMedal} size="2x" style={{color: medal.color}}></FontAwesomeIcon>
-                    <Typography className={medal.name + "Medals"} variant="h5" component="div">
-                        {medal.count}
-                    </Typography>
-                </Container>
-            )
-        })
-        let medalButtons = this.state.medals.map((medal) => {
+        let medalButtons = this.props.medals.map((medal) => {
             let upperMedalName = medal.name.slice(0,1).toUpperCase() + medal.name.slice(1);
             return (
-                <Button key={medal.name + "Button"} className={"add" + upperMedalName + "MedalButton"} onClick={() => {this.props.addMedal(this.props.name, medal.name)}} color="info" variant="contained" sx={{margin: 1}}>
-                    Add a {upperMedalName} Medal!
+            <Grid item xs={4} key={medal.name + "Medal"}>
+                <FontAwesomeIcon icon={faMedal} size="2x" style={{color: medal.color}}></FontAwesomeIcon>
+                <Typography className={medal.name + "Medals"} variant="h5" component="div">
+                    {medal.count}
+                </Typography>
+                <Button key={medal.name + "ButtonPlus"} className={"add" + upperMedalName + "MedalButton"} onClick={() => {this.props.changeMedalNum(this.props.name, medal.name, 1)}} color="info" variant="contained" sx={{margin: 1}}>
+                    <FontAwesomeIcon icon={faPlus} />
                 </Button>
+                <Button key={medal.name + "ButtonMinus"} className={"add" + upperMedalName + "MedalButton"} disabled={!medal.count > 0} onClick={() => {this.props.changeMedalNum(this.props.name, medal.name, -1)}} color="info" variant="contained" sx={{margin: 1}}>
+                    <FontAwesomeIcon icon={faMinus} />
+                </Button>
+            </Grid>
             )
         })
 
@@ -36,10 +29,11 @@ class Country extends Component {
                 <Box className="countryMedals"  sx={{bgcolor: 'secondary.main', color: 'secondary.contrastText', borderRadius: '10px', 
                 border: 3, borderColor: '#ba68c8'}}>
                     <Typography className="countryName" variant="h3" component="div" gutterBottom marginBottom={0}>
-                        {this.state.name}
+                        {this.props.name}
                     </Typography>
-                    {medals}
-                    {medalButtons}
+                    <Grid container>
+                        {medalButtons}
+                    </Grid>
                 </Box>
             </Grid>
         );
