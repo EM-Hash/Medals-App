d41da6de74135e5dc591b1e9fe098ec080dbe105
EM-Hash <emeade3@my.wctc.edu>
2022-08-19 10:27:54 -0500

    List top 3 countries, based on a score assigned to each medal

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import { Box} from '@mui/system';
 import Country from './Country';
 import CountryModal from './CountryModal';
@@ -16,7 +16,60 @@ class Countries extends Component {
         ],
     } 
 
-    addMedal = (countryName, medalName) => {
+    getTopCountries = () => {
+        //Sort by each country's score (described in getScore)
+        //Make a new array that will have the ordered countries
+        let topCountries = [];
+        //For each country...
+        for(let country of this.state.countries){
+            //Get the medal count
+            let score = this.getScore(country.medals);
+            //Create a new object w/ the name and total medal count
+            let newCountry = {"country": country.name, "score": score};
+            //Add the new object to the countries
+            topCountries = topCountries.concat([newCountry,]);
+        }
+        //Sort the countries by score (descending)
+        topCountries.sort((a, b) => {
+            return a.score > b.score ? -1 : 1;
+        });
+        //For the top three countries...
+        let length = topCountries.length < 3 ? topCountries.length : 3;
+        let countryList = [];
+        for(let i = 0; i < length; i++){
+            let country = topCountries[i];
+            //List the country and its score
+            countryList[i] = (
+                <li key={country.country}>
+                    {country.country}: {country.score} pts
+                </li>
+            );
+        }
+        let list = (
+            <ol>
+                {countryList.map((country) => {return country})}
+            </ol>
+        )
+        return list;
+    }
+
+    getScore = (medals) => {
+        //Score based on the medals each country gained
+        //Each medal has a weighted value
+        let values = {"gold": 5, "silver": 3, "bronze": 1};
+        let sum = 0;
+        //For each tier of medal...
+        medals.map(medal => {
+            //Calculate the points earned
+            let points = values[medal.name] * medal.count;
+            //Add it to the sum of total points
+            sum += points;
+            return sum;
+        });
+        return sum;
+    }
+
+    changeMedalNum = (countryName, medalName, quantity) => {
         let countries = this.state.countries;
         let targetCountry = countries.filter(country => country.name === countryName);
         if(targetCountry.length > 0){
@@ -24,7 +77,7 @@ class Countries extends Component {
             let targetMedal = newCountry.medals.filter(medal => medal.name === medalName);
             let targetMedalIndex = newCountry.medals.indexOf(targetMedal[0]);
             let countryIndex = countries.indexOf(targetCountry[0]);
-            newCountry.medals[targetMedalIndex].count++;
+            newCountry.medals[targetMedalIndex].count += quantity;
             countries[countryIndex] = newCountry;
             this.setState({countries: countries});
         }
@@ -47,14 +100,21 @@ class Countries extends Component {
         let countries = this.state.countries;
         let countryMedals = countries.map((country) => {
             return (
-                <Country key={country.name} name={country.name} medals={country.medals} addMedal={this.addMedal}></Country>
+                <Country key={country.name} name={country.name} medals={country.medals} changeMedalNum = {this.changeMedalNum}></Country>
             )
         });
-
         return (
         <Box sx={{flexGrow: 1}}>
-            <Grid container spacing={2} marginTop = {0} padding={2} >
-              {countryMedals}
+            <Grid container justifyContent="center">
+                <Grid item xs={6}>
+                    <Paper sx={{pb: 1, px: 10, mt: 1}}>
+                        <h2>Top Countries: </h2>
+                        {this.getTopCountries()}
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} container spacing={2} marginTop = {0} padding={2} >
+                    {countryMedals}
+                </Grid>
             </Grid>
             <CountryModal addCountry = {this.addCountry}></CountryModal>
         </Box>
